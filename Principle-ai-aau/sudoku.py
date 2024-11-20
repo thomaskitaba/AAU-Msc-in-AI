1105df759042ff0a07b1feee7e8a216bdee04c73
thomaskitaba <thomas.kitaba@gmail.com>
2024-11-20 13:18:23 +0300

    sudoku

--- Principle-ai-aau/sudoku.py
+++ Principle-ai-aau/sudoku.py
@@ -1,22 +1,59 @@
 #!/usr/bin/python3
-
+import math
+    
 def is_valid(board, row, col, num):
-    return False
+    """ check if placing num on (row, col) is valid """
+    # Check column
+    for i in range(9):
+        if board[i][col] == num or board[row][i] == num:
+            return False
+    # Check part of sudoku (3 x 3) grid
 
+    start_col = 3 * math.floor(col / 3)   # 3 * math.floor(col // 3)
+    start_row = 3 * (row // 3)
+    for i in range(start_row, start_row + 3):
+        for j in range(start_col, start_col + 3):
+            if board[i][j] == num:
+                return False
+    return True
+
+    
 def find_empty_space(board):
-    row = col = 0
-    return (row, col)
+    """ return row, col if empty location 
+    to place num is found 
+    """
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] == 0:
+                return (r, c)
+    return None
+            
 
 def solve_sudoku(board):
     empty = find_empty_space(board)
-    # place num 1 - 9  to this location
-    board[row] = col
-    num = 1
-    if is_valid(board, row, col, num):
-        return True
+    if empty:
+        row, col = empty # destructure the returned values
     else:
-        return False
+        return True  # The board is full therefore it is solved
+    # Place num 1 - 9  to this location
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+            if solve_sudoku(board):
+                return True
+            board[row][col] = 0
     
+    return False
+def print_board(board):
+    for r in range(9):
+        for c in range(9):
+            if c != 9:
+                print(board[r][c], end=" | ")
+        print("")
+        
+# def print_board(board):
+#     for r in board:
+#         print(r)
 if __name__ == "__main__":
     board = [
     [5, 3, 0, 0, 7, 0, 0, 0, 0],
@@ -32,6 +69,7 @@ if __name__ == "__main__":
     
     if (solve_sudoku(board)):
         print("Solved board")
+        print_board(board)
     else:
         print("Board is unsolvable")
     
